6223741ab93523dbeae24affbaec007165dd537f
Hamilton - Naja <hamilton.oliveira@najasolucoes.com.br>
2017-12-20 22:36:43 -0300

    dados faker

--- features/step_definitions/student_step.rb
+++ features/step_definitions/student_step.rb
@@ -1,3 +1,5 @@
+require 'faker'
+
 Given(/^I launch web site$/) do
   visit('/')
 end
@@ -6,16 +8,17 @@ And(/^I click New Student$/) do
   click_link('New Student')
 end
 
-And(/^I enter register (.*)$/) do |value|
-  fill_in 'student[register]', with: value
+#And(/^I enter register (.*)$/) do |value|
+And(/^I enter register$/) do
+  fill_in 'student[register]', with: Faker::Number.number(10)
 end
 
-And(/^I enter name (.*)$/) do |value|
-  fill_in 'student[name]', with: value
+And(/^I enter name$/) do
+  fill_in 'student[name]', with: Faker::Name.name
 end
 
-And(/^I enter email (.*)$/) do |value|
-  fill_in 'student[email]', with: value
+And(/^I enter email$/) do
+  fill_in 'student[email]', with: Faker::Internet.email
 end
 
 When(/^I submit form$/) do
--- features/student.feature
+++ features/student.feature
@@ -5,9 +5,9 @@ Feature: Student page
   Scenario: Create new student
     Given I launch web site
     And I click New Student
-    And I enter register 123456
-    And I enter name Hamilton
-    And I enter email hamilton@teste.com.br
+    And I enter register
+    And I enter name
+    And I enter email
     When I submit form
     Then I see success message
 
